5374efaa2009538d3dc8985f22273fd486fbc9af
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-09-19 22:56:02 -0300

    Excluindo colunas, e categorizando os dados

--- main.py
+++ main.py
@@ -12,4 +12,11 @@ plt.rcParams['figure.figsize'] = (15, 6)
 plt.style.use('seaborn-darkgrid')
 
 base_dados = pd.read_csv('house_data.csv')
-print(base_dados.head())
+
+# Excluindo colunas
+base_dados.drop(columns=['total (R$)', 'fire insurance (R$)'], inplace=True)
+
+#Tipos das colunas
+Colunas_Categoricas = base_dados.columns[base_dados.dtypes == object]
+Colunas_Numericas = base_dados.columns[base_dados.dtypes != object]
+print(Colunas_Numericas, Colunas_Categoricas)
